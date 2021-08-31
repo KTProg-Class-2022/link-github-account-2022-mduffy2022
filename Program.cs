870c1ff6bfbdd84b72922c73c5e4d80fb381f21f
Michael Duffy <michaelduffy14@outlook.com>
2021-08-31 08:54:22 -0400

    print my name

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace HelloClass
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Mr. Lesieur!");
+            Console.WriteLine("Hello Michael!");
+            Console.WriteLine("test");
         }
     }
 }
